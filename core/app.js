9234146609c884486800c161bf196895e3ef19c7
Lubov Golikova <lubovgolikova@MacBook-Pro-Lubov.local>
2020-11-22 22:55:07 +0200

    add first changes

--- core/app.js
+++ core/app.js
@@ -1,15 +1,14 @@
 const express = require('express');
-
-// const {apiRouter, notFoundRouter, productsRouter} = require("../core/routes");
+const {apiRouter, notFoundRouter, productsRouter} = require("../core/routes");
 const {sequelize} = require('./dataBase')
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded())
 
-// app.use('/api', apiRouter)
-// app.use('*', notFoundRouter)
-// app.use('/', productsRouter)
+app.use('/api', apiRouter)
+app.use('*', notFoundRouter)
+app.use('/', productsRouter)
 sequelize.sync({alter: true})
     .then(() => app.listen(5000, err => err && console.log(err) || console.log('Listen 5000 ...')))
     .catch(console.log)
